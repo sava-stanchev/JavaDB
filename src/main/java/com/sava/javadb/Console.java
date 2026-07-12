521f53156aa0481d5f014f1c14e1915548b5bcb5
sava-stanchev <s.stanchev94@gmail.com>
2026-07-12 20:25:43 +0300

    add basic key-value operations

--- src/main/java/com/sava/javadb/Console.java
+++ src/main/java/com/sava/javadb/Console.java
@@ -18,24 +18,57 @@ public class Console {
         while (true) {
             System.out.print("> ");
             String input = sc.nextLine().trim();
-
             if (input.isEmpty())
                 continue;
-            if (input.equalsIgnoreCase("exit"))
-                break;
-            if (input.equalsIgnoreCase("help")) {
-                showHelp();
-                continue;
+            String[] parts = input.split("\\s+");
+            String cmd = parts[0].toUpperCase();
+
+            switch (cmd) {
+                case "PUT":
+                    if (parts.length == 3) {
+                        db.put(parts[1], parts[2]);
+                        System.out.println("OK");
+                    } else {
+                        System.out.println("Usage: PUT <key> <value>");
+                    }
+
+                    break;
+                case "GET":
+                    if (parts.length == 2) {
+                        String val = db.get(parts[1]);
+                        System.out.println(val != null ? val : "Key not found.");
+                    } else {
+                        System.out.println("Usage: GET <key>");
+                    }
+
+                    break;
+                case "DELETE":
+                    if (parts.length == 2) {
+                        boolean isDeleted = db.delete(parts[1]);
+                        System.out.println(isDeleted ? "OK" : "Key not found.");
+                    } else {
+                        System.out.println("Usage: DELETE <key>");
+                    }
+
+                    break;
+                case "HELP":
+                    showHelp();
+                    continue;
+                case "EXIT":
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Unknown command.");
+                    System.out.println("Type HELP to see available commands.");
             }
-
-            System.out.println("You entered: " + input);
         }
-
-        System.out.println("Goodbye!");
     }
 
     private void showHelp() {
         System.out.println("Available commands:");
+        System.out.println("PUT <key> <value> - Store a value");
+        System.out.println("GET <key> - Retrieve a value");
+        System.out.println("DELETE <key> - Delete a key");
         System.out.println("HELP - Show available commands");
         System.out.println("EXIT - Exit JavaDB");
     }
