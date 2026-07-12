55854a2ad50f3ec1005f5f28d24da5c7159a05a9
sava-stanchev <s.stanchev94@gmail.com>
2026-07-12 17:11:14 +0300

    add user input handling to console

--- src/main/java/com/sava/javadb/Console.java
+++ src/main/java/com/sava/javadb/Console.java
@@ -1,7 +1,17 @@
 package com.sava.javadb;
 
+import java.util.Scanner;
+
 public class Console {
+    private final Scanner sc;
+
+    public Console() {
+        sc = new Scanner(System.in);
+    }
+
     public void start() {
         System.out.println("Welcome to JavaDB!");
+        String input = sc.nextLine();
+        System.out.println("You entered: " + input);
     }
 }
