364b14e448f61d9796d0b1ba8943ae27fc4e0bed
sava-stanchev <s.stanchev94@gmail.com>
2026-07-12 19:18:36 +0300

    add basic console commands

--- src/main/java/com/sava/javadb/Console.java
+++ src/main/java/com/sava/javadb/Console.java
@@ -15,14 +15,26 @@ public class Console {
 
         while (true) {
             System.out.print("> ");
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
+            if (input.isEmpty())
+                continue;
             if (input.equalsIgnoreCase("exit"))
                 break;
+            if (input.equalsIgnoreCase("help")) {
+                showHelp();
+                continue;
+            }
 
             System.out.println("You entered: " + input);
         }
 
         System.out.println("Goodbye!");
+    }
+
+    private void showHelp() {
+        System.out.println("Available commands:");
+        System.out.println("HELP - Show available commands");
+        System.out.println("EXIT - Exit JavaDB");
     }
 }
