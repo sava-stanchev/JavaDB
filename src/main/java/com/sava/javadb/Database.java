3d11167f12dee9ebb8ca2423a2529652ed158741
sava-stanchev <s.stanchev94@gmail.com>
2026-07-13 21:59:52 +0300

    implement database save and load

--- src/main/java/com/sava/javadb/Database.java
+++ src/main/java/com/sava/javadb/Database.java
@@ -1,6 +1,11 @@
 package com.sava.javadb;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Database {
@@ -20,5 +25,25 @@ public class Database {
 
     public boolean delete(String key) {
         return data.remove(key) != null;
+    }
+
+    public void save(Path path) throws IOException {
+        List<String> lines = new ArrayList<>();
+
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            lines.add(entry.getKey() + "=" + entry.getValue());
+        }
+
+        Files.write(path, lines);
+    }
+
+    public void load(Path path) throws IOException {
+        data.clear();
+        List<String> lines = Files.readAllLines(path);
+
+        for (String line : lines) {
+            String[] parts = line.split("=");
+            data.put(parts[0], parts[1]);
+        }
     }
 }
