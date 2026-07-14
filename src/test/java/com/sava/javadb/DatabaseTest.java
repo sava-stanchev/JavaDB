1782342730cb160dca1fa0985a331c4ad4d648d5
sava-stanchev <s.stanchev94@gmail.com>
2026-07-14 20:24:51 +0300

    add persistence tests

--- src/test/java/com/sava/javadb/DatabaseTest.java
+++ src/test/java/com/sava/javadb/DatabaseTest.java
@@ -2,11 +2,17 @@ package com.sava.javadb;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
     private Database db;
+    @TempDir
+    Path tempDir;
 
     @BeforeEach
     void setUp() {
@@ -45,5 +51,29 @@ public class DatabaseTest {
         db.delete("name");
         String val = db.get("name");
         assertNull(val);
+    }
+
+    @Test
+    void shouldSaveAndLoadDatabase() throws IOException {
+        db.put("name", "Sava");
+        db.put("city", "Sofia");
+        Path file = tempDir.resolve("database.db");
+        db.save(file);
+        Database db2 = new Database();
+        db2.load(file);
+        assertEquals("Sava", db2.get("name"));
+        assertEquals("Sofia", db2.get("city"));
+    }
+
+    @Test
+    void shouldClearExistingDataBeforeLoad() throws IOException {
+        db.put("name", "Sava");
+        Path file = tempDir.resolve("database.db");
+        db.save(file);
+        Database db2 = new Database();
+        db2.put("oldKey", "oldVal");
+        db2.load(file);
+        assertNull(db2.get("oldKey"));
+        assertEquals("Sava", db2.get("name"));
     }
 }
